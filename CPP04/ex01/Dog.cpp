514a1ba3c1bc9fdcbb5dada07cd8fb13de54838f
amber <swaynmg@gmail.com>
2026-03-12 18:15:23 +0100

    comments

--- CPP04/ex01/Dog.cpp
+++ CPP04/ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: mgregoir <mgregoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 17:36:27 by mgregoir          #+#    #+#             */
-/*   Updated: 2026/03/06 17:13:34 by mgregoir         ###   ########.fr       */
+/*   Updated: 2026/03/12 18:13:20 by mgregoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ Dog::Dog() {
 // CONSTRUCTEUR DE COPIE (Deep Copy)
 Dog::Dog(const Dog& other) : Animal(other) {
 	std::cout << "Dog copy constructor called" << std::endl;
-    this->_brain = new Brain(*other._brain); // On cree un NOUVEAU cerveau
+    this->_brain = new Brain(*other._brain);   // On cree un NOUVEAU cerveau
 }
 
 // OPERATEUR D'ASSIGNATION (Deep Copy)
@@ -29,10 +29,8 @@ Dog& Dog::operator=(const Dog& other) {
 	std::cout << "Dog assignment operator called" << std::endl;
     if (this != &other) {
 		this->type = other.type;
-        // On supprime l'ancien cerveau pour eviter les fuites
-        delete this->_brain;
-        // On en cree un nouveau avec le contenu de l'autre
-        this->_brain = new Brain(*other._brain);
+        delete this->_brain;      // On supprime l'ancien cerveau pour eviter les fuites
+        this->_brain = new Brain(*other._brain);    // On en cree un nouveau avec le contenu de l'autre
     }
     return *this;
 }
