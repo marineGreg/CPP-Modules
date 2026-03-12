d2e3544cd4eba84fff4049ed317bc6239b989ea2
marineGreg <marinegregoire1@gmail.com>
2026-03-12 15:35:33 +0100

    AAnimal

--- CPP04/ex02/AAnimal.cpp
+++ CPP04/ex02/AAnimal.cpp
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   AAnimal.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgregoir <mgregoir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amber <amber@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 16:12:33 by mgregoir          #+#    #+#             */
-/*   Updated: 2026/03/06 11:06:49 by mgregoir         ###   ########.fr       */
+/*   Updated: 2026/03/12 15:30:57 by amber            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AAnimal.hpp"
 
 AAnimal::AAnimal() : type("Animal") {
-    std::cout << "Animal default constructor called" << std::endl;
+    std::cout << "AAnimal default constructor called" << std::endl;
 }
 
 AAnimal::AAnimal(const AAnimal& other) {
-    std::cout << "Animal copy constructor called" << std::endl;
+    std::cout << "AAnimal copy constructor called" << std::endl;
     *this = other;
 }
 
 AAnimal& AAnimal::operator=(const AAnimal& other) {
-    std::cout << "Animal assignment operator called" << std::endl;
+    std::cout << "AAnimal assignment operator called" << std::endl;
     if (this != &other)
         this->type = other.type;
     return *this;
 }
 
 AAnimal::~AAnimal() {
-    std::cout << "Animal destructor called" << std::endl;
+    std::cout << "AAnimal destructor called" << std::endl;
 }
 
 // makeSound() n'est plus defini car c'est une methode virtuelle pure
