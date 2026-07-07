3ab2cb66b09e77a720a6a7b1c66d82828ccf9289
marineGreg <marinegregoire1@gmail.com>
2026-07-07 18:26:25 +0200

    ex01

--- CPP06/ex01/Data.hpp
+++ CPP06/ex01/Data.hpp
@@ -7,7 +7,6 @@ struct Data
 {
 	int	id;
 	std::string name;
-	std::string user;
 };
 
 #endif
--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -2,45 +2,43 @@
 #include "Serializer.hpp"
 
 int main() {
-    // 1. On crée notre objet Data avec des informations
+    // 1. On cree notre objet Data avec des informations
     Data originalData;
     originalData.id = 42;
     originalData.name = "Theodore Vicente";
 
-    // On récupère son adresse mémoire (le pointeur)
+    // On recupere son adresse memoire (le pointeur)
     Data* originalPtr = &originalData;
 
-    std::cout << "==========================================" << std::endl;
-    std::cout << "--- 1. ÉTAT ORIGINAL ---" << std::endl;
-    std::cout << "Adresse mémoire : " << originalPtr << std::endl;
+    std::cout << "===== 1. ETAT ORIGINAL =====" << std::endl;
+    std::cout << "Adresse memoire : " << originalPtr << std::endl;
     std::cout << "ID              : " << originalPtr->id << std::endl;
     std::cout << "Nom             : " << originalPtr->name << std::endl;
-    std::cout << "==========================================" << std::endl << std::endl;
 
-    // 2. SÉRIALISATION (Pointeur -> Entier)
-    std::cout << "[... Sérialisation en cours ...]" << std::endl;
+    // 2. SERIALISATION (Pointeur -> Entier)
+    std::cout << "\n[..... Serialisation en cours .....]" << std::endl;
     uintptr_t raw = Serializer::serialize(originalPtr);
     
-    // On affiche l'entier brut (tu verras, c'est un très grand nombre !)
-    std::cout << "Valeur brute (uintptr_t) : " << raw << std::endl << std::endl;
+    // On affiche l'entier brut
+    std::cout << "Valeur brute (uintptr_t) : " << raw << std::endl;
 
-    // 3. DÉSÉRIALISATION (Entier -> Pointeur)
-    std::cout << "[... Désérialisation en cours ...]" << std::endl;
+    // 3. DESERIALISATION (Entier -> Pointeur)
+    std::cout << "\n[..... Deserialisation en cours .....]" << std::endl;
     Data* deserializedPtr = Serializer::deserialize(raw);
 
-    std::cout << "==========================================" << std::endl;
-    std::cout << "--- 2. ÉTAT APRÈS DÉSÉRIALISATION ---" << std::endl;
-    std::cout << "Adresse mémoire : " << deserializedPtr << std::endl;
+    std::cout << "\n===== 2. ETAT APRES DESERIALISATION =====" << std::endl;
+    std::cout << "Adresse memoire : " << deserializedPtr << std::endl;
     std::cout << "ID              : " << deserializedPtr->id << std::endl;
     std::cout << "Nom             : " << deserializedPtr->name << std::endl;
-    std::cout << "==========================================" << std::endl << std::endl;
 
-    std::cout << "--- 3. VERDICT ---" << std::endl;
+    std::cout << "\n===== 3. VERDICT =====" << std::endl;
     if (originalPtr == deserializedPtr) {
-        std::cout << "SUCCÈS : Les deux adresses mémoires sont STRICTEMENT identiques !" << std::endl;
-        std::cout << "   Original : " << originalPtr << " == Récupéré : " << deserializedPtr << std::endl;
+        std::cout << "SUCCES : Les deux adresses memoires sont STRICTEMENT identiques !"
+		<< std::endl;
+        std::cout << "Original : " << originalPtr << " == Recupere : " << deserializedPtr
+		<< std::endl;
     } else {
-        std::cout << "ÉCHEC : Les adresses ne correspondent pas !" << std::endl;
+        std::cout << "ECHEC : Les adresses ne correspondent pas !" << std::endl;
     }
 
     return 0;
